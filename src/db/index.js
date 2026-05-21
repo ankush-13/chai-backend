6911f518d6823a5f36256c88545afd38eefb70c2
Ankush Patle <ankushpatle321@gmail.com>
2026-05-21 21:44:02 +0530

    bugs fixed mongoerror

--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,18 @@ import { DB_Name } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
-        console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}${DB_Name}`
+        );
+
+        console.log(
+            `MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+        );
+
     } catch (error) {
         console.log("MONGODB Connection Error", error);
-        process.exit(1)
+        process.exit(1);
     }
-}
+};
 
-export default connectDB
+export default connectDB;
